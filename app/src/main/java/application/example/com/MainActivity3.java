1dc78d6b1244ce2b0cf661feb96f522781567b9e
Mateusz Adamczyk <matada200060@gmail.com>
2021-12-02 19:25:51 +0100

    1.1

--- app/src/main/java/application/example/com/MainActivity3.java
+++ app/src/main/java/application/example/com/MainActivity3.java
@@ -2,13 +2,73 @@ package application.example.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    private SeekBar seekBar; //
+    private int brightness;//
+    private ContentResolver contentResolver;//
+    private Window window;//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        seekBar = findViewById(R.id.seekBar);//
+        contentResolver = getContentResolver();//
+        window = getWindow();//
+
+        seekBar.setMax(255);//
+        seekBar.setKeyProgressIncrement(1);//
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(Settings.System.canWrite(this)){
+                Toast.makeText(this,"You can change brightness", Toast.LENGTH_LONG).show();
+            }else{
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + getApplication().getPackageName()));
+                startActivity(intent);
+            }
+        }//
+
+        try {
+            brightness = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);
+            seekBar.setProgress(brightness);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }//
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                brightness = progress;
+                Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+                WindowManager.LayoutParams layoutParams = window.getAttributes();
+                layoutParams.screenBrightness = brightness / (float)300;
+                window.setAttributes(layoutParams);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
